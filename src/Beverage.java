522b43f99eb08ee7b7fb2b453872c216b93c3c1c
Krupkolllia <krupkoilla397@gmail.com>
2026-02-02 19:07:25 +0100

    Refactor Beverage.java class
    Make it abstract. Delete not needed condiments methods & variables. Heading on to the decorator pattern

--- src/Beverage.java
+++ src/Beverage.java
@@ -1,50 +1,11 @@
-public class Beverage {
-    protected String description;
-
-    private boolean milk = false;
-    private boolean soy = false;
-    private boolean mocha = false;
-    private boolean whip = false;
-
-    public String getDescription() { return description; }
+public abstract class Beverage {
+    protected String description = "Unknown Beverage";
 
 
-    public double cost() {
-        return (milk ? 0.59 : 0)
-                + (soy ? 0.79 : 0)
-                + (mocha ? 0.99 : 0)
-                + (whip ? 0.69 : 0);
+    public String getDescription() {
+        return description;
     }
 
-    public boolean hasMilk() {
-        return milk;
-    }
+    public abstract double cost();
 
-    public void setMilk(boolean milk) {
-        this.milk = milk;
-    }
-
-    public boolean hasSoy() {
-        return soy;
-    }
-
-    public void setSoy(boolean soy) {
-        this.soy = soy;
-    }
-
-    public boolean hasMocha() {
-        return mocha;
-    }
-
-    public void setMocha(boolean mocha) {
-        this.mocha = mocha;
-    }
-
-    public boolean hasWhip() {
-        return whip;
-    }
-
-    public void setWhip(boolean whip) {
-        this.whip = whip;
-    }
 }
