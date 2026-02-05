b3ccfaed24eaca83eb26af586a7f9769a43e519b
Krupkolllia <krupkoilla397@gmail.com>
2026-02-05 22:02:31 +0100

    Rework abstract Pizza class

--- src/abstracts/Pizza.java
+++ src/abstracts/Pizza.java
@@ -1,24 +1,23 @@
 package abstracts;
 
+import ingredients.*;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public abstract class Pizza {
-    protected String name;
-    protected String dough;
-    protected String sauce;
-    protected ArrayList<String> toppings = new ArrayList<>();
+    String name;
+    Dough dough;
+    Sauce sauce;
+    Veggies[] veggies;
+    Cheese cheese;
+    Pepperoni pepperoni;
+    Clams clam;
 
 
-    protected void prepare() {
-        System.out.println("Preparing " + name);
-        System.out.println("Tossing dough: " + dough);
-        System.out.println("Adding sauce: " + sauce);
-        System.out.println("Adding toppings: ");
-        for (String topping : toppings) {
-            System.out.println("  " + topping);
-        }
+    protected abstract void prepare();
 
-    }
 
     protected void bake() {
         System.out.println("Baking for 25 minutes at 350");
@@ -32,7 +31,29 @@ public abstract class Pizza {
         System.out.println("Place pizza in official abstracts.PizzaStore box");
     }
 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        String veggiesList = veggies != null && veggies.length > 0
+                ? Arrays.stream(veggies)
+                .map(Object::toString)
+                .collect(Collectors.joining(", "))
+                : "none";
+
+        return name + ": \n" +
+                "Dough: " + dough + "\n" +
+                "Sauce: " + sauce + "\n" +
+                "Veggies: " + veggiesList + "\n" +
+                "Cheese: " + cheese + "\n" +
+                "Pepperoni: " + pepperoni + "\n" +
+                "Clams: " + clam;
     }
 }
