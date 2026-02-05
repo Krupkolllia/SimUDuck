b156ebcadf80cccf9fe9c50e88bdd249bd4a575c
Krupkolllia <krupkoilla397@gmail.com>
2026-02-05 15:15:44 +0100

    Add the toppings option to Pizzas

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,25 +1,33 @@
+import java.util.ArrayList;
+
 public abstract class Pizza {
     String name;
     String dough;
     String sauce;
+    ArrayList<String> toppings = new ArrayList<>();
 
 
     public void prepare() {
         System.out.println("Preparing " + name);
         System.out.println("Tossing dough: " + dough);
         System.out.println("Adding sauce: " + sauce);
+        System.out.println("Adding toppings: ");
+        for (String topping : toppings) {
+            System.out.println("  " + topping);
+        }
+
     }
 
     public void bake() {
-        System.out.println("Baking " + name + " for 25 minutes at 350");
+        System.out.println("Baking for 25 minutes at 350");
     }
 
     public void cut() {
-        System.out.println("Cutting " + name + " into slices");
+        System.out.println("Cutting the pizza into slices");
     }
 
     public void box() {
-        System.out.println("Boxing " + name);
+        System.out.println("Place pizza in official PizzaStore box");
     }
 
     public String getName() {
