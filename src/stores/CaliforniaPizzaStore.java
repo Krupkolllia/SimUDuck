767556aa00939e91f6e30f38eef37b6787b95673
Krupkolllia <krupkoilla397@gmail.com>
2026-02-07 20:28:43 +0100

    Final version of the app

--- src/stores/CaliforniaPizzaStore.java
+++ src/stores/CaliforniaPizzaStore.java
@@ -2,13 +2,14 @@ package stores;
 
 import abstracts.Pizza;
 import abstracts.PizzaStore;
-import jdk.jshell.spi.ExecutionControl;
 
 public class CaliforniaPizzaStore extends PizzaStore {
 
     @Override
     protected Pizza createPizza(String type) {
-        // To be implemented...
+        /* TODO: add the CaliforniaPizzaIngredientFactory
+            then implement this method
+        */
         return null;
     }
 }
